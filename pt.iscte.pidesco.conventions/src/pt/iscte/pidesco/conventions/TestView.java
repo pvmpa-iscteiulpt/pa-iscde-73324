f9850f4803d303d716fe4024c1094bcfab025e02
pvmpa-iscteiulpt <pvmpa@iscte-iul.pt>
2019-12-09 16:30:19 +0000

    first commit

---
+++ pt.iscte.pidesco.conventions/src/pt/iscte/pidesco/conventions/TestView.java
@@ -0,0 +1,22 @@
+package pt.iscte.pidesco.conventions;
+
+import java.util.Map;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+
+import pt.iscte.pidesco.extensibility.PidescoView;
+
+public class TestView implements PidescoView {
+
+	public TestView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
