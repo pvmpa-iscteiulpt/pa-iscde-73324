24a3026cba21e862e03e5a7cc7ef774e52e58546
pvmpa-iscteiulpt <pvmpa@iscte-iul.pt>
2019-12-09 16:46:07 +0000

    hello world

--- pt.iscte.pidesco.conventions/src/pt/iscte/pidesco/conventions/TestView.java
+++ pt.iscte.pidesco.conventions/src/pt/iscte/pidesco/conventions/TestView.java
@@ -2,8 +2,12 @@ package pt.iscte.pidesco.conventions;
 
 import java.util.Map;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 import pt.iscte.pidesco.extensibility.PidescoView;
 
@@ -15,7 +19,11 @@ public class TestView implements PidescoView {
 
 	@Override
 	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
-		// TODO Auto-generated method stub
+		viewArea.setLayout(new RowLayout(SWT.HORIZONTAL));
+		Label label = new Label(viewArea, SWT.NONE);
+		label.setImage(imageMap.get("smiley.png"));
+		Text text = new Text(viewArea, SWT.BORDER);
+		text.setText("Hello World");
 
 	}
 
