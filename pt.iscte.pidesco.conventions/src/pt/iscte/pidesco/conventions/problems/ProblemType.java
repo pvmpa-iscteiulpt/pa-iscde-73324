6723e598ddd8541aeb55f7e629cfb8e1e66adb39
pvmpa-iscteiulpt <pvmpa@iscte-iul.pt>
2019-12-14 14:06:32 +0000

    Removed nonsense on ProblemType javadoc

--- pt.iscte.pidesco.conventions/src/pt/iscte/pidesco/conventions/problems/ProblemType.java
+++ pt.iscte.pidesco.conventions/src/pt/iscte/pidesco/conventions/problems/ProblemType.java
@@ -3,8 +3,7 @@ package pt.iscte.pidesco.conventions.problems;
 public interface ProblemType {
 	/**
 	 * Defines a proper name for the Problem Type, so it can be sorted out by the
-	 * Conventions program without much hassle. It's like toString, but it's better
-	 * to make this behaviour explicit.
+	 * Conventions program without much hassle.
 	 * 
 	 * @return The problem's name.
 	 */
